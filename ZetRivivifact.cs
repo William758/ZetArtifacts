71a77522a221c36f38a596ae8aab04d36ef03094
William758 <williamreyes758@gmail.com>
2021-09-18 18:46:48 -0700

    Require Living Player

--- ZetRivivifact.cs
+++ ZetRivivifact.cs
@@ -1,73 +1,76 @@
-﻿using RoR2;
+using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
 
 namespace TPDespair.ZetArtifacts
 {
-    public static class ZetRevivifact
-    {
-        internal static void Init()
-        {
-            ZetArtifactsPlugin.RegisterLanguageToken("ARTIFACT_ZETREVIVIFACT_NAME", "Artifact of Revival");
-            ZetArtifactsPlugin.RegisterLanguageToken("ARTIFACT_ZETREVIVIFACT_DESC", "Dead players respawn after the boss is defeated.");
+	public static class ZetRevivifact
+	{
+		internal static void Init()
+		{
+			ZetArtifactsPlugin.RegisterLanguageToken("ARTIFACT_ZETREVIVIFACT_NAME", "Artifact of Revival");
+			ZetArtifactsPlugin.RegisterLanguageToken("ARTIFACT_ZETREVIVIFACT_DESC", "Dead players respawn after the boss is defeated.");
 
-            SaveDeathPositionHook();
-            RevivalHook();
-        }
-
-
-
-        private static void SaveDeathPositionHook()
-        {
-            On.RoR2.CharacterMaster.OnBodyDeath += (orig, self, body) =>
-            {
-                if (NetworkServer.active)
-                {
-                    if (RunArtifactManager.instance.IsArtifactEnabled(ZetArtifactsContent.Artifacts.ZetRevivifact))
-                    {
-                        self.transform.position = body.footPosition;
-                        self.transform.rotation = body.transform.rotation;
-                    }
-                }
-
-                orig(self, body);
-            };
-        }
-
-        private static void RevivalHook()
-        {
-            On.RoR2.Run.OnServerBossDefeated += (orig, self, bossGroup) =>
-            {
-                orig(self, bossGroup);
-
-                if (NetworkServer.active)
-                {
-                    if (RunArtifactManager.instance.IsArtifactEnabled(ZetArtifactsContent.Artifacts.ZetRevivifact))
-                    {
-                        foreach (NetworkUser networkUser in NetworkUser.readOnlyInstancesList)
-                        {
-                            if (networkUser.isActiveAndEnabled)
-                            {
-                                CharacterMaster master = networkUser.master;
-
-                                if (master.IsDeadAndOutOfLivesServer() || !master.GetBody() || !master.GetBody().healthComponent.alive)
-                                {
-                                    RespawnAtDeathPoint(master);
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-        }
+			SaveDeathPositionHook();
+			RevivalHook();
+		}
 
 
 
-        private static void RespawnAtDeathPoint(CharacterMaster master)
-        {
-            Vector3 position = master.transform.position;
-            Quaternion rotation = master.transform.rotation;
-            master.Respawn(position, rotation);
-        }
-    }
+		private static void SaveDeathPositionHook()
+		{
+			On.RoR2.CharacterMaster.OnBodyDeath += (orig, self, body) =>
+			{
+				if (NetworkServer.active)
+				{
+					if (RunArtifactManager.instance.IsArtifactEnabled(ZetArtifactsContent.Artifacts.ZetRevivifact))
+					{
+						self.transform.position = body.footPosition;
+						self.transform.rotation = body.transform.rotation;
+					}
+				}
+
+				orig(self, body);
+			};
+		}
+
+		private static void RevivalHook()
+		{
+			On.RoR2.Run.OnServerBossDefeated += (orig, self, bossGroup) =>
+			{
+				orig(self, bossGroup);
+
+				if (NetworkServer.active)
+				{
+					if (RunArtifactManager.instance.IsArtifactEnabled(ZetArtifactsContent.Artifacts.ZetRevivifact))
+					{
+						if (Run.instance && Run.instance.livingPlayerCount > 0)
+						{
+							foreach (NetworkUser networkUser in NetworkUser.readOnlyInstancesList)
+							{
+								if (networkUser.isActiveAndEnabled)
+								{
+									CharacterMaster master = networkUser.master;
+
+									if (master.IsDeadAndOutOfLivesServer() || !master.GetBody() || !master.GetBody().healthComponent.alive)
+									{
+										RespawnAtDeathPoint(master);
+									}
+								}
+							}
+						}
+					}
+				}
+			};
+		}
+
+
+
+		private static void RespawnAtDeathPoint(CharacterMaster master)
+		{
+			Vector3 position = master.transform.position;
+			Quaternion rotation = master.transform.rotation;
+			master.Respawn(position, rotation);
+		}
+	}
 }
